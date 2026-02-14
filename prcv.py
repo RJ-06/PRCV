9018f53e2d9fe4eabff2daca205608c30fc1bfad
DaveyNavy <davidyzhu2@gmail.com>
2026-02-13 16:24:52 -0800

    Add docs

--- prcv.py
+++ prcv.py
@@ -1,25 +1,28 @@
 from collections import defaultdict
 import random
 
-def election(ballots: list[list], OPEN_POSITIONS: int) -> list[int]:
+def election(ballots: list[list[int]], OPEN_POSITIONS: int) -> list[int]:
     """
-    ballots: list of ballots
-    OPEN_POSITIONS: number of open positions
+    ballots: list of ballots. Has shape (N, M), with N votes 
+            cast and M total candidates. ballot[i][j] is the 
+            jth-choice candidate on ballot i
+    OPEN_POSITIONS: number of positions up for election
 
     Returns:
-        winners
+        winners: list[int]
     """
 
     candidate_vote_counts = defaultdict(int)
     candidate_ballots = defaultdict(list)
 
-    for ballot in ballots:
-        candidate_vote_counts[ballot[0]] += 1
-        candidate_ballots[ballot[0]].append(ballot)
-
     THRESHOLD = len(ballots) // (OPEN_POSITIONS + 1) + 1
 
     winners = []
+
+    # Count all ballots and determine if anyone has a majority
+    for ballot in ballots:
+        candidate_vote_counts[ballot[0]] += 1
+        candidate_ballots[ballot[0]].append(ballot)
 
     for candidate in candidate_vote_counts.keys():
         if (candidate_vote_counts[candidate] >= THRESHOLD):
@@ -40,14 +43,14 @@ def election(ballots: list[list], OPEN_POSITIONS: int) -> list[int]:
                 candidate_ballots[candidate_to_transfer_to].append(ballot)
                 candidate_vote_counts[candidate_to_transfer_to] += 1
 
-        num_winners = len(winners)
         # Add new winners
+        num_winners = len(winners)
         for candidate in candidate_vote_counts.keys():
             if (candidate_vote_counts[candidate] >= THRESHOLD and candidate not in winners):
                 winners.append(candidate)
-        
+
+        # Eliminate the last-place candidate 
         if len(winners) == num_winners:
-            # Eliminate the last-place candidate
             lowest_ranked_candidate = min(candidate_vote_counts, key=candidate_vote_counts.get)
             for ballot in candidate_ballots[lowest_ranked_candidate]:
                 candidate_to_transfer_to = ballot[ballot.index(lowest_ranked_candidate) + 1]
